f91696bd80fbc83b9789dd155c9f53f72addd393
derpen <rizqyassabil@hotmail.com>
2024-11-14 16:51:21 +0700

    Removing target

--- timer-gui/target/debug/deps/timer_gui-84fc19c42011fa23.d
+++
@@ -1,6 +0,0 @@
-/mnt/windows/Code/rust-collections/timer-gui/target/debug/deps/timer_gui-84fc19c42011fa23: src/main.rs src/countdown.rs
-
-/mnt/windows/Code/rust-collections/timer-gui/target/debug/deps/timer_gui-84fc19c42011fa23.d: src/main.rs src/countdown.rs
-
-src/main.rs:
-src/countdown.rs:
